ae4f9f3285c936431d62bea5da38e0d286d551bf
snigdhadutt07-sudo <snigdhadutt07@gmail.com>
2026-04-18 04:30:25 +0530

    Add files via upload

---
+++ Assignment1.c
@@ -0,0 +1,26 @@
+// Assignment 1
+/*Write a program in C to check leap year.*/
+#include <stdio.h>
+
+int main() {
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+    }
+    else if (year % 100 == 0) {
+        printf("%d is not a leap year.\n", year);
+    }
+    else if (year % 4 == 0) {
+        printf("%d is a leap year.\n", year);
+    }
+    else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
+}
+S
